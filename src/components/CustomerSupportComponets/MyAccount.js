ffdcbd8d4cfd98359412eca38ed03857dc2cda60
robiulalam76 <robiulalam762400@gmail.com>
2023-05-01 11:19:01 +0600

    coustomer support responsive updated

--- src/components/CustomerSupportComponets/MyAccount.js
+++ src/components/CustomerSupportComponets/MyAccount.js
@@ -16,13 +16,13 @@ const MyAccount = () => {
 
             <div>
                 <div className='w-full h-16 bg-primary flex justify-between items-center rounded-tl-3xl rounded-tr-3xl mt-8 px-4'>
-                    <div className='w-full flex justify-start gap-4'>
-                        <Image className='w-5' src={dot} alt="" />
-                        <h1 className='text-white text-xl'>What can I do if I forgot my password?</h1>
+                    <div className='w-full flex items-center justify-start gap-4'>
+                        <Image className='w-5 h-5' src={dot} alt="" />
+                        <h1 className='text-white md:text-xl'>What can I do if I forgot my password?</h1>
                     </div>
                     <Image className='w-8' src={upArrow} alt="" />
                 </div>
-                <p className='text-xl text-gray-500 my-6'>Lorem ipsum dolor sit amet consectetur. Diam id risus mi faucibus. Id commodo egestas orci neque eget hendrerit maecenas. Quis pretium habitant risus risus neque mauris ornare. Malesuada.</p>
+                <p className='text-sm md:text-xl text-gray-500 my-2 md:my-6'>Lorem ipsum dolor sit amet consectetur. Diam id risus mi faucibus. Id commodo egestas orci neque eget hendrerit maecenas. Quis pretium habitant risus risus neque mauris ornare. Malesuada.</p>
             </div>
 
             <div className='grid grid-cols-1 gap-2 cursor-pointer'>
@@ -30,17 +30,17 @@ const MyAccount = () => {
                     populerTopics.map(topic => (
                         <div className='h-fit w-full'>
                             <div onClick={() => dispatch(setViewAnsware(viewAnsware ? '' : topic.id))}
-                                key={topic.id} className={`w-full h-fit py-4 flex justify-between items-center px-4
+                                key={topic.id} className={`w-full h-fit py-2 md:py-4 flex justify-between items-center px-2 md:px-4
                                 ${viewAnsware === topic.id ? 'bg-gray-100' : 'hover:bg-gray-100'}`}>
                                 <div className='flex justify-start gap-4 items-center'>
                                     <Image className='w-4' src={circle} alt="" />
-                                    <h1 className='text-xl text-black font-semibold'>{topic.question}</h1>
+                                    <h1 className='md:text-xl text-black font-semibold'>{topic.question}</h1>
                                 </div>
                                 <Image className='w-8' src={downArrow} alt="" />
                             </div>
                             {
                                 viewAnsware === topic.id && <div className='bg-gray-100'>
-                                    <p className='text-gray-900 text-xl px-4 py-3'>{topic.answare}</p>
+                                    <p className='text-gray-900 md:text-xl px-2 md:px-4 py-2 md:py-3'>{topic.answare}</p>
                                 </div>
                             }
                         </div>
--- src/pages/customer-support/index.js
+++ src/pages/customer-support/index.js
@@ -12,7 +12,7 @@ const CustomerSupportMain = () => {
     return (
         <section className='bg-white min-h-screen'>
             <div className='w-full h-80 relative'>
-                <Image className='w-full h-full' src={img1} alt="" />
+                <Image className='w-full h-full object-cover' src={img1} alt="" />
                 <div className='absolute top-0 h-full w-full flex items-center justify-center px-4'>
                     <div className='mx-auto w-full md:w-[600px] h-full flex items-center'>
                         <input className='flex-grow h-12 w-full focus:outline-none border px-2' type="search" name="search" placeholder='What do you need?' />
@@ -22,14 +22,14 @@ const CustomerSupportMain = () => {
                     </div>
                 </div>
             </div>
-            <div className='max-w-[1440px] mx-auto mt-8 px-4'>
-                <div className='flex md:justify-between items-center flex-wrap md:flex-nowrap bg-gray-100 border h-20 w-full'>
+            <div className='max-w-[1440px] mx-auto mt-8 md:px-4 px-2'>
+                <div className='flex md:justify-between items-center flex-wrap md:flex-nowrap bg-gray-100 border h-fit w-full'>
                     {
-                        categories.map((ctg, i) => <button onClick={() => dispatch(setSelectedCategory(ctg.id))}
+                        categories?.map((ctg, i) => <button onClick={() => dispatch(setSelectedCategory(ctg.id))}
                             key={i}
-                            className={`w-fit px-4 md:w-full h-20 flex justify-center items-center
+                            className={`px-4 w-full h-12 flex justify-center items-center
                             ${selectedCategory === ctg.id ? 'bg-primary text-white' : 'hover:bg-primary hover:text-white text-black'}`}>
-                            <h1 className='font-semibold text-xl'>{ctg.title}</h1>
+                            <h1 className='font-semibold'>{ctg.title}</h1>
                         </button>
                         )
                     }
@@ -37,6 +37,15 @@ const CustomerSupportMain = () => {
 
                 {
                     selectedCategory === '1' && <MyAccount />
+                }
+                {
+                    selectedCategory === '2' && <MyAccount />
+                }
+                {
+                    selectedCategory === '3' && <MyAccount />
+                }
+                {
+                    selectedCategory === '4' && <MyAccount />
                 }
             </div>
         </section>
